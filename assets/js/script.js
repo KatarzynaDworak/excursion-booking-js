c9bfcd710675051b6a001fb686c5b8e450dbf4c7
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-19 12:54:25 +0100

    task partly done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,101 @@
-// KOSNULTACJE
-//1 podział wiersza - nie wiem czy dobrze, bo w konsoli nie widać
-//2 dodanie wycieczek do newLi + dataset + display: none
+// KONSULTACJE
 
 const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
 "2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
 
 console.log( txt.split(/[\r\n]+/gm) );
 
-//1. obsługa wybierania pliku przez użytkownika
-//2. pobranie zawartości pliku
-
 const inputElement = document.querySelector('input[type="file"]');
-console.log(inputElement);
+// console.log(inputElement);
 
 const ulElement = document.querySelector('.excursions');
-console.log(ulElement);
+// console.log(ulElement);
 
-// inputElement.addEventListener('change', handleFile);
-
-ulElement.addEventListener('change', handleFile);
+inputElement.addEventListener('change', handleFile);
 function handleFile(event) {
     const file = event.target.files[0];
     
-    if(file && file.type.includes('file')) {
+    if(file && file.type === 'text/csv') {
         const reader = new FileReader();
+        reader.readAsText(file);
         
-        // podział tej zawartości na wiersze (wycieczki)
         reader.onload = function(readerEvent) {
+            
+            readerEvent.preventDefault();
             const contents = readerEvent.target.result;
-            console.log('contents', readerEvent.target.result); // nic się nie wyświetla
+            // console.log('contents', readerEvent.target.result); 
             const offers = contents.split(/[\r\n]+/gm);
-            console.log('offers', offers); // nic się nie wyświetla
-            //PROBLEM nie wiem czy są offers, bo się nie wyświetlają
+            // console.log('offers', offers); 
 
-            // podział wiersza na poszczególne elementy  (id, nazwa itp.) KOLUMNY
-            const id = offers.indexOf['id'];
-            const name = offers.indexOf['name'];
-            const description = offers.indexOf['description??'];
-            const priceAdult = offers.indexOf['??'];
-            const priceChild = offers.indexOf['??'];
+            offers.forEach(function(offer) {
+                const excursion = offer.split('\",\"'); // ok TABLICA
+                // console.log(excursion); 
 
-        }
-        
+                const id = excursion[0]; //stringi do innerText
+                const name = excursion[1];
+                const description = excursion[2];
+                const priceAdult = excursion[3];
+                const priceChild = excursion[4];
+
+                const excursionObj = {id, name, description, priceAdult, priceChild}; // każda wycieczka to obiekt
+                // console.log(excursionObj);
+                
+                createPrototype(excursionObj);
+                createBasket(excursionObj) // 2 tablice, w kazdej 1 obiekt
+                placeOrder();
+            })
+        } 
     }
 }
 
-// utworzenie odpowiednich elementów HTML 
-// wypełnienie ich danymi 
+// KONSULTACJE: jak UKRYĆ pierwsze <li>?
+const liElement = document.querySelector('li');
+// liElement.style.display = 'none';
 
+function createPrototype(obj) {
 
-// dodanie ich do drzewa DOM
-const newLi = document.querySelector('.excursions__item--prototype').cloneNode(true);
-console.log(prototype);
+    const newLi = document.querySelector('.excursions__item--prototype').cloneNode(true);
+    console.log(newLi); //2 newLi
+    
+    const newTitle = newLi.querySelector('.excursions__title');
+    newTitle.innerText = obj['name'];
+    
+    const newDescription = newLi.querySelector('.excursions__description');
+    newDescription.innerText = obj['description']; //DZIAŁA
+    
+    ulElement.appendChild(newLi);
+    
+    // const newPriceAdult = newLi.querySelector('.'); //KONSULTACJE
+    // newPriceAdult.innerText = obj['priceAdult'];
+    
+    // const newPriceChild = newLi.querySelector('.'); //KONSULTACJE
+    // newPriceChild.innerText = obj['priceChild'];
+    
+}
 
-ulElement.appendChild(newLi);
-ulElement.appendChild(newLi);
-ulElement.appendChild(newLi);
-ulElement.appendChild(newLi);
-ulElement.appendChild(newLi);
+//KOSZYK I DODAWANIE DO NIEGO WYCIECZEK
+//1. po kliknięciu w 'dodaj do zamówienia':
+//walidujemy: czy wpisano liczbę od 0 w górę
+//Jeśli tak to: 
+// 2. dodajemy do listy w koszyku [name + suma ceny + przycisk 'X', child ilość + cena, adult ilość + cena]
+// 3. powiększamy sumę w KOSZYKU
+// 4. przed wysłaniem zamowienia sprawdzamy, czy 'inię + nazwisko + email' nie są puste + email ma @
 
+function createBasket(obj) {
+
+    const basket = [];
+    basket.push(obj);
+
+    const  
+
+}
+
+function placeOrder() {
+    const submitBtn = document.querySelector('.order__field-submit'); //ok
+    console.log(submitBtn);
+
+    submitBtn.addEventListener('submit', function() {
+        alert('Dziękujemy za złożenie zamówienia. Potwierdzenie zamówienia zostanie wysłanie mailem.')
+    })
+
+}
