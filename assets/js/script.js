c9db9adcf35fa86d948f63c4e08cadcfed20e767
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-03-22 20:41:09 +0100

    task partly done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 // KONSULTACJE
+//1. /FUNKCJA - linia 100 - powoduje BŁĄD lub gdzieś jest błąd, który powoduje, 
+//ze znikają wycieczki po kliknięciu w 'dodaj do zamówienia'
+// 2. jak poukładać callbacki np linia 116, 122 (funkcje nie zostały wywołane jeszcze)
 
 // const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
 // "2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
@@ -85,93 +88,65 @@ function createPrototype(obj) { // copyPrototype
     // CENA DZIECKO
     const priceChild = parseInt(priceElements[1].innerText); // 50 number
     // console.log('priceAdult => ', priceAdult, 'priceChild => ', priceChild);
-    
-    //** */  DO USUNIĘCIA - NASŁUCHIWANIE NA FORM
-    // 2 PRZYCISKI - DODAJ DO ZAMÓWIENIA
-    // const submitBtn = newLi.querySelector('.excursions__field-input--submit');
-    // console.log('submitBtn', submitBtn);
-
-    // ROZRÓŻNIAMY PRZYCISKI - DODAJ DO ZAMÓWIENIA - DLA WYCIECZEK Z PLIKU
-    // const trips = [];
-    // trips.push(excursionName);
-    // console.log(trips); // 2 TABLICE KAZDA ZAWIERA 1 POZYCJĘ NAZWĘ WYCIECZKI
-    //dla rozróżnienia wycieczek zostały dodane atrybuty data-name='nazwa wycieczki'
-    // **
 
     //NASŁUCHIWANIE NA FORM
     const form = document.querySelector('form');
     console.log(form);
 
-    form.addEventListener('submit', function(event) { 
-        event.preventDefault();
-        console.log('ok');
-        // const userInput = event.target.value;
-    })
-   
+    form.addEventListener('submit', getUserInput);
+}
+
+//FUNKCJA powoduje BŁĄD lub gdzieś jest błąd, który powoduje, e znikają wycieczki po kliknięciu w 'dodaj do zamówienia'
+function getUserInput(event) {
+    event.preventDefault();
+    console.log('ok');
+    
+    const userInput = event.target.value; //array?
+    const numberOfChild = parseInt(userInput[1]);
+    const numberOfAdults = parseInt(userInput[0]);
+    console.log('number adults => ', numberOfAdults, 'number childs => ', numberOfChild)
+
     // // WPISANA PRZEZ UŻYTKOWNIKA ilość dzieci
     // userInputList[1].value;
     // // wpisana ilość rodziców
     // userInputList[0].value;
-    
-
-
-    //PROTOTYP SUMMARY
-    const summaryElement = document.querySelector('.summary__item--prototype').cloneNode(true);
-    // console.log(summaryElement);
-    
-    //KOLEJNE ETAPY + POTRZEBNE DANE:
-
-    // NAZWA WYCIECZKI DO SUMMARY
-    const summaryName = summaryElement.querySelector('.summary__name');
-    // console.log(summaryName);
-    //dodaję nazwę wycieczki wybranej w klonie w summary
-    summaryName.innerText = excursionName;
-    // dodaję ilości osób w wybranej wycieczce w klonie w summary
-    const summaryPricePerPerson = summaryElement.querySelector('.summary__prices');
-    // console.log(summaryPricePerPerson);
-    //pobieram ilość osób wpisanych w formularzu
-    //pobranie userInput z obu inputów
-    const userInputList = newLi.querySelectorAll('.excursions__field-input');
-    // console.log(userInputList);
-    // submit
-    const submitToAdd = userInputList[2];
-
-    // wyliczamy sumę = ilość dorosłych x cena dorosły, ilość dzieci [nasłuchiwanie na submit + to co wpisał uzytkownik e.target.value] x cena dziecko newElements[1]
-
-    const addToChartBtn = document.querySelectorAll('.excursions__field-input');
-    // console.log(addToChartBtn);
 }
 
-function addToSummary(event, priceAdult, priceChild) {
-    event.preventDefault();
-    const numberOfAdults = event.target.value;
-    console.log(event.target.value);
-    const numberOfChild = event.target.value;
-    console.log(numberOfAdults);
-    //wylicz sumę
+//funkcja nie została wywolana
+function getSum(priceAdult, numberOfAdults, priceChild, numberOfChild) {
     const sum = (priceAdult * numberOfAdults) + (priceChild * numberOfChild);
-    //dodaj pozycję do koszyka
-
-    // console.log('tu ma być funkcja, która dodaje wycieczkę do koszyka')
+    return sum;
 }
 
+//funkcja nie została wywolana
+function createSummary(trip, tripName, numberOfAdults, numberOfChild, priceAdult, priceChild) {
+    // const basket = [];
+    // basket.push(trip);
+    const summaryPanel = document.querySelector('.panel__summary');
+    const summaryElement = document.querySelector('.summary__item--prototype');
+    summaryElement.style.display = 'none';
+    
+    const newSummaryElement = summaryPanel.cloneNode(true);
+    newSummaryElement.style.display = 'block';
+    
+    summaryPanel.appendChild(newSummaryElement);
+    console.log(summaryPanel);
 
-// function createBasket(obj) {
+    //DODAJĘ WYCIECZKI DO SUMMARY
+    // DODAJĘ TYTUŁ
+    const summaryName = newSummaryElement.querySelector('.summary__name');
+    summaryName.innerText = tripName;
 
-//     const basket = [];
-//     basket.push(obj);
+    //DODAJĘ SUMĘ DO SUMMARY
+    const totalPrice = newSummaryElement.querySelector('.summary__total-price');
+    totalPrice.innerText = sum + 'PLN'; // czy to się połączy na np. 199PLN?
 
-//     const excursionAddBtn = document.querySelector('.excursions__field-input--submit') 
-//     // console.log(excursionAddBtn); //btn liElement
+    // DODAJĘ ILOŚĆ DOROSŁYCH I DZIECI
+    const summaryPrice = newSummaryElement.querySelector('.summary__prices');
+    summaryPrice.innerText = 'dorośli: ' + numberOfAdults + ' x ' + priceAdult + ' PLN ' + ' , dzieci: ' + numberOfChild + ' x ' + priceChild + 'PLN';
 
-// }
-
-// function placeOrder() {
-//     const submitBtn = document.querySelector('.order__field-submit'); //ok
-//     console.log(submitBtn);
-
-//     submitBtn.addEventListener('submit', function() {
-//         alert('Dziękujemy za złożenie zamówienia. Potwierdzenie zamówienia zostanie wysłanie mailem.')
-//     })
-
-// }
+    //DODAJĘ SUMĘ DO PANEL ORDER
+    const panelOrder = document.querySelector('.panel__order');
+    const orderTotalPriceValue = panelOrder.querySelector('.order__total-price-value');
+    orderTotalPriceValue.innerText = totalPrice + 'PLN';
+}
