12f909f3bc6a2ecc04adc36f43ec529e0234cdc5
Katarzyna Dworak <k.dw@poczta.onet.pl>
2024-04-04 14:17:00 +0200

    task done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//DO POPRAWY - wczytuje się 2x ta sama wycieczka, nie wczytuje się ojców + linie 85, 87
+//DO POPRAWY - linie 100 - w całkowitej sumie wpisuje sumę ostatniej wycieczki, a nie dodaje sum wszystkich wycieczek
 
 const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
 "2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
@@ -21,9 +21,10 @@ function handleFile(event) {
             
             readerEvent.preventDefault();
             const contents = readerEvent.target.result;
-
+            
             const offers = contents.split(/[\r\n]+/gm);
-
+            // console.log(offers);
+            
             offers.forEach(function(offer) {
                 const excursion = offer.split('\",\"'); 
 
@@ -50,6 +51,19 @@ function copyPrototype(obj) {
     newLi.style.display = 'block';
     newLi.classList.add('excursions__item--trip');
     
+    //dodajemy wszystkie elementy 
+    const title = newLi.querySelector('.excursions__title');
+    // const id = newLi.querySelector('.excursions__title');
+    const description = newLi.querySelector('.excursions__description');
+    const priceAdult = newLi.querySelector('.excursions__price');
+    // const priceChild = newLi.querySelector('.excursions__title');
+
+    // console.log(title);
+    title.innerText = obj.name;
+    description.innerText = obj.description;
+    priceAdult.innerText = obj.priceAdult;
+    // console.log(title);
+    
     ulElement.appendChild(newLi);
     
     const LiList = document.querySelectorAll('.excursions__item--trip');
@@ -63,7 +77,7 @@ function copyPrototype(obj) {
 
 function handleSubmit(event) {
     event.preventDefault();
-    console.log(event, event.target);
+    // console.log(event, event.target);
 
     const formElement = event.target;
     const adult = parseInt(formElement.elements[0].value);
@@ -73,25 +87,30 @@ function handleSubmit(event) {
     const adultPrice = parseInt(prices[0].innerText);
     const childPrice = parseInt(prices[1].innerText);
 
-    const sum = (adult * adultPrice) + (child * childPrice);
+    const sum = (adult * adultPrice) + (child * childPrice); //number
 
     const tripName = event.target.previousElementSibling.querySelector('h2').innerText; 
-    console.log( 'nazwa=> ', tripName); 
+    // console.log( 'nazwa=> ', tripName); 
 
     createSummaryItem(adult, child, adultPrice, childPrice, tripName, sum);
 
     const orderTotalPrice = document.querySelector('.order__total-price');
     const finalChart = orderTotalPrice.querySelector('.order__total-price-value');
-    finalChart.style.display = 'none';
+    finalChart.innerText = '';
+    finalChart.innerText += sum;
+    // console.log(finalChart);
+    // finalChart.style.display = 'none';
     
-    const newFinalChart = finalChart.cloneNode(true); // BŁĄD: DODAJĄ SIĘ KLONY KAZDEJ WYCIECZKI
-    newFinalChart.innerText = '';
-    newFinalChart.innerText += sum + 'PLN'; // BŁĄD: KADA SUMA WYCIECZKI DODAJE SIĘ W OSOBNYM KLONIE - BRAK CAŁKOWITEJ SUMY
-    orderTotalPrice.appendChild(newFinalChart);
-    newFinalChart.style.display = 'block';
 
-    const totalPrice = document.querySelector('.order__total-price-value').innerText += sum;
+    // const newFinalChart = finalChart.cloneNode(true); // BŁĄD: DODAJĄ SIĘ KLONY KAZDEJ WYCIECZKI
+    // newFinalChart.innerText = '';
+    // newFinalChart.innerText += sum + 'PLN'; // BŁĄD: KADA SUMA WYCIECZKI DODAJE SIĘ W OSOBNYM KLONIE - BRAK CAŁKOWITEJ SUMY
+    // orderTotalPrice.appendChild(newFinalChart);
+    // newFinalChart.style.display = 'block';
+
+    // const totalPrice = document.querySelector('.order__total-price-value').innerText += sum;
     // newFinalChart.querySelector('.order__total-price-value').innerText = sum + 'PLN';
+    // return sum;
 }
 
 function createSummaryItem(adult, child, adultPrice, childPrice, tripName, sum) {
@@ -114,5 +133,5 @@ function createSummaryItem(adult, child, adultPrice, childPrice, tripName, sum) 
     // const orderTotalPrice = document.querySelector('.order__total-price-value').innerText = sum + 'PLN';
 
     summaryPanel.appendChild(newSummaryElement);
-    console.log(summaryPanel);
+    // console.log(summaryPanel);
 }
